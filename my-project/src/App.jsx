326bcbf831aa6d5450e73c74f4a0a77fd9d5fa62
titouanmoulin <titouan.moulin@etu.unilim.fr>
2024-10-17 09:07:07 +0200

    importer

---
+++ my-project/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import useFetch from './hook/useFetch'
+
+export default function App() {
+  const [searchSerie, setSearchSerie] = useState("breaking bad");
+
+  const {
+    data: showData,
+    isLoading: isLoadingShow,
+    error: showError,
+  } = useFetch('https://api.tvmaze.com/singlesearch/shows?q="breaking bad"');
+
+  console.log(showData);
+  
+  if (isLoadingShow) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+
+  return (
+
+    <div>
+    </div>
+  )
+}
