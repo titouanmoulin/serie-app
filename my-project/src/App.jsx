07cd1e6c2caa3260357feac6a9cb23e67dddf726
Axel <axel.a.rameau@gmail.com>
2024-10-24 18:23:51 +0200

    Save affichage 1 série page accueil

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState, useEffect } from "react";
 import useFetch from './hook/useFetch'
 import { Button } from "@/components/ui/button"
+import { Badge } from "@/components/ui/badge"
+
 
 
 export default function App() {
-  const searchserieurl = "https://api.tvmaze.com/singlesearch/shows?q="
-  const [searchSerie, setSearchSerie] = useState(searchserieurl +"squid game");
+
+  const searchserie1 = "https://api.tvmaze.com/shows"
+  const [searchSerie, setSearchSerie] = useState(searchserie1);
 
   let handlerSubmit = (e) => {
     e.preventDefault();
     const filmUrl = new FormData(e.target).get("film");
-    setSearchSerie(searchserieurl + filmUrl)
+    setSearchSerie(searchserie1 + filmUrl)
   };
 
 
@@ -19,39 +22,70 @@ export default function App() {
     isLoading: isLoadingShow,
     error: showError,
   } = useFetch(searchSerie);
+  
+  
+  console.log(showData);
+
+  const backgroundImageUrl = showData && showData.image ? showData.image.original : '';
 
   return (
-    <>
+    <article className="bg-background h-screen"> 
+      <nav className="bg-gradient-to-b from-black to-pink-500/0 flex justify-between items-center py-5 px-10">
+        <img className="w-20" src="/logo-flopvideo.svg" alt="Bande de flopeurs" />
+        <div className="py-2 pl-5 pr-2 h-fit rounded-xl bg-white border-white border-2">
+            <form onSubmit={handlerSubmit}>
+            <input
+              className="bg-white focus:outline-none focus:ring-0 placeholder:text-black text-black"
+              type="text"
+              name="film"
+              placeholder="Rechercher"
 
-      <form onSubmit={handlerSubmit}>
-        <input
-          className="border-black border-2"
-          type="text"
-          name="film"
-          placeholder="Search for a series..."
+            />
+            <Button size="icon" variant="ghost" type="submit"><svg className="w-16 h-16" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 24" strokeWidth={1.5} stroke="currentColor" >
+              <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+            </svg></Button>
+          </form>
+        </div>
+      </nav>
 
-        />
-        <Button type="submit"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-        </svg></Button>
-        {/* <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-        </svg>
-        </button> */}
-      </form>
+   <section 
+      className={`h-4/5 w-full`}
+      style={{ backgroundImage: `url(${backgroundImageUrl})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+      <div className="flex flex-col gap-10 items-start justify-end h-full pl-10 pb-20 bg-gradient-to-t from-black/100 via-black/0 to-pink-500/0">
+        <h1 className="text-8xl font-bold">{showData.name}</h1>
+        <div className="flex gap-5">
+          {showData.genres.map((genre,index) => (
+            <Badge variant="default" id={index}> {genre} </Badge>
+            ))}
+        </div>  
+      </div>
+    </section>
+
+
+    <section className="flex flex-col gap-5">
+      <h2 className="font-medium text-3xl"> Tendances </h2>
+        {showData && showData.image.map((image,index) => (
+          <img id={index} src={image.original} alt={showData.name} />
+        ))}
+    </section>
+
+
       <div>
         {isLoadingShow && <p>isloading</p>}
         {showData &&
           <div>
             <div>
               <img src={showData.image.medium} alt={showData.name} />
-            </div>
+             </div>
           </div>
         }
         {showError && <p>non</p>}
       </div>
 
 
-    </>
+          <section>
+
+          </section>
+    </article>
   )
 }
--- my-project/src/components/ui/button.jsx
+++ my-project/src/components/ui/button.jsx
@@ -10,21 +10,21 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+          "bg-primary text-primary-foreground shadow rounded-full px-5 py-2",
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-primary/20 hover:text-accent-secondary hover:text-primary text-primary",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        icon: "h-8 w-8 text-center",
       },
     },
     defaultVariants: {
