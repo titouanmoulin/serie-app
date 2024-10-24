cf6cfce2f67c69eb357a98a491ff7debab7cf27b
titouanmoulin <titouan.moulin@etu.unilim.fr>
2024-10-24 09:23:24 +0200

    install shadcn

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import useFetch from './hook/useFetch'
+import { Button } from "@/components/ui/button"
+
 
 export default function App() {
   const searchserieurl = "https://api.tvmaze.com/singlesearch/shows?q="
@@ -29,10 +31,13 @@ export default function App() {
           placeholder="Search for a series..."
 
         />
-        <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+        <Button type="submit"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg></Button>
+        {/* <button type="submit"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
         </svg>
-        </button>
+        </button> */}
       </form>
       <div>
         {isLoadingShow && <p>isloading</p>}
